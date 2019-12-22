60c1b95799520f4f49e96e2d43273c86f756d5e0
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-12-22 13:18:14 +0800

    update pom.xml file

---
+++ hsf-rpc/src/main/java/com/laibao/dubbo/hsf/rpc/benchmark/RequestObject.java
@@ -0,0 +1,22 @@
+package com.laibao.dubbo.hsf.rpc.benchmark;
+import java.io.Serializable;
+/**
+ * Just for RPC Benchmark Test,request object
+ * 
+ */
+public class RequestObject implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private byte[] bytes = null;
+	
+	public RequestObject(int size){
+		bytes = new byte[size];
+	}
+
+	public byte[] getBytes() {
+		return bytes;
+	}
+	
+	
+}
---
+++ hsf-rpc/src/main/java/com/laibao/dubbo/hsf/rpc/benchmark/ResponseObject.java
@@ -0,0 +1,21 @@
+package com.laibao.dubbo.hsf.rpc.benchmark;
+import java.io.Serializable;
+/**
+ * Just for RPC Benchmark Test,response object
+ */
+public class ResponseObject implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private byte[] bytes = null;
+	
+	public ResponseObject(int size){
+		bytes = new byte[size];
+	}
+
+	public byte[] getBytes() {
+		return bytes;
+	}
+	
+	
+}
