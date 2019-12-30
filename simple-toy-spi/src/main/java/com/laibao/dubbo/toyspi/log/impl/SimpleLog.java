61cf155c165d7b624a82e89f8d07d38907f3d0f2
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-12-30 11:56:08 +0800

    update pom.xml file

---
+++ simple-toy-spi/src/main/java/com/laibao/dubbo/toyspi/log/impl/SimpleLog.java
@@ -0,0 +1,11 @@
+package com.laibao.dubbo.toyspi.log.impl;
+
+
+import com.laibao.dubbo.toyspi.log.Log;
+
+public class SimpleLog implements Log {
+
+    public void say() {
+        System.out.println("com.laibao.dubbo.toyspi.log.impl.SimpleLog");
+    }
+}
---
+++ simple-toy-spi/src/main/java/com/laibao/dubbo/toyspi/log/impl/ToyLog.java
@@ -0,0 +1,11 @@
+package com.laibao.dubbo.toyspi.log.impl;
+
+
+import com.laibao.dubbo.toyspi.log.Log;
+
+public class ToyLog implements Log {
+
+    public void say() {
+        System.out.println("com.laibao.dubbo.toyspi.log.impl.ToyLog");
+    }
+}
